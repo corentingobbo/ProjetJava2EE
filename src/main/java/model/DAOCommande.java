b1b61300dd74b7efd6034ef49caba72b6ca3ea21
pedago <pedago@pcalb-mm0420.univ-jfc.fr>
2019-12-04 14:50:43 +0100

    dao

--- src/main/java/model/DAOCommande.java
+++ src/main/java/model/DAOCommande.java
@@ -30,7 +30,8 @@ public class DAOCommande {
         return null;
         
     }
-    public CommandeEntity newCommande (ClientEntity client){
+    /**
+    public void newCommande (ClientEntity client) throws SQLException{
         
           ///num derniere ligne 
           String sql="SELECT COUNT(*) FROM Commande";
@@ -40,23 +41,56 @@ public class DAOCommande {
           } catch (SQLException ex) {
             Logger.getLogger(DAOCommande.class.getName()).log(Level.SEVERE, null, ex);
         }
-          String sql2 = "SELECT DATE( NOW() )"; 
-          try(Connection co = ds.getConnection();
-              java.sql.Statement stm = co.createStatement();
-              ResultSet rs2 = stm.executeQuery(sql2)){              
+          Date auj = new Date();
+          
+          
+          String sql3 = "INSERT INTO commande VALUES(?,?,?,?,0,?,?,?,?,?,?,0)";
+           try(Connection co = ds.getConnection();
+              PreparedStatement stm = co.prepareStatement(sql3);)
+              { 
+                  stm.setInt(1,rs.getInt(1)+1);
+                  stm.setClientEntity(2,client);
+                  stm.setDate(3,auj);
+                  stm.setDate(4,auj);
+                  stm.setClientEntity(5,client);
+                  stm.setString(6,client.getAdresse_livraison);
+                  stm.setString(7,client.getVille_livraison);
+                  stm.setString(8,client.getRegion_livraison);
+                  stm.setString(9,client.getCode_postal_livraison);
+                  stm.setString(10,client.getPays_livraison);
+                  stm.executeUpdate();
+              }             
           } catch (SQLException ex) {
             Logger.getLogger(DAOCommande.class.getName()).log(Level.SEVERE, null, ex);
-        }
-          
-         // destinataire,adresse,ville,region,code,pays: client
-         //remise
-         
-      
-        return new CommandeEntity(rs,this.client,rs2,rs2);
-                
+                 
     }
     public void validerCommande(){
         
     };
+    
+    public void ajouterLigne(CommandeEntity commande,ProduitEntity produit, int quantite){
+        commande.getPort() += produit.getPrix_Unitaire * quantite;
+        String sqlp="UPDATE commande SET port = ?";
+        
+        
+        String sql = "INSERT INTO ligne VALUES(?,?,?)";
+         try(Connection co = ds.getConnection();
+              PreparedStatement stm = co.prepareStatement(sql);)
+              { 
+                  stm.setInt(1,commande);
+                  stm.setClientEntity(2,produit);
+                  stm.setDate(3,quantite);
+                  
+                  stm.executeUpdate();
+              }             
+          } catch (SQLException ex) {
+            Logger.getLogger(DAOCommande.class.getName()).log(Level.SEVERE, null, ex);            
+    }
+        
+        return new ligneEntity(commande,produit,quantite);
+    }
+    public CommandeEntity rechercheCommmandeParClient(){
+        return null;
+    }**/
     
 }
