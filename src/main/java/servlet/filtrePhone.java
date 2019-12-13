20e1448cc5dc72cbc8e66bca9cab3659d50c325b
cgobbo <corentin.gobbo@hotmail.fr>
2019-12-13 23:29:12 +0100

    Communication entre serveur et index.html ok
    Enfin.............

---
+++ src/main/java/servlet/filtrePhone.java
@@ -0,0 +1,135 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlet;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Properties;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.DAOProduit;
+import model.DataSourceFactory;
+
+/**
+ *
+ * @author Corentin
+ */
+@WebServlet(name = "filtrePhone", urlPatterns = {"/filtrePhone"})
+public class filtrePhone extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        DAOProduit dao = new DAOProduit(DataSourceFactory.getDataSource());
+        // Properties est une Map<clé, valeur> pratique pour générer du JSON
+        Properties resultat = new Properties();
+
+        ArrayList<String> phones = new ArrayList<>();
+
+        if (request.getParameter("Apple") != null) {
+            phones.add(request.getParameter("Apple"));
+        }
+        if (request.getParameter("Samsung") != null) {
+            phones.add(request.getParameter("Samsung"));
+        }
+        if (request.getParameter("Huawei") != null) {
+            phones.add(request.getParameter("Huawei"));
+        }
+        if (request.getParameter("Oneplus") != null) {
+            phones.add(request.getParameter("Oneplus"));
+        }
+        if (request.getParameter("Xiaomi") != null) {
+            phones.add(request.getParameter("Xiaomi"));
+        }
+        if (request.getParameter("Asus") != null) {
+            phones.add(request.getParameter("Asus"));
+        }
+        if (request.getParameter("Sony") != null) {
+            phones.add(request.getParameter("Sony"));
+        }
+        if (request.getParameter("Nokia") != null) {
+            phones.add(request.getParameter("Nokia"));
+        }
+        String prixMin = "";
+        if (request.getParameter("prixMin") != null) {
+            prixMin = request.getParameter("prixMin");
+        }
+        String prixMax = "";
+        if (request.getParameter("prixMax") != null) {
+            prixMax = request.getParameter("prixMax");
+        }
+
+        resultat.put("phones", phones);
+        resultat.put("prixMin", prixMin);
+        resultat.put("prixMax", prixMax);
+
+        try (PrintWriter out = response.getWriter()) {
+            // On spécifie que la servlet va générer du JSON
+            response.setContentType("application/json;charset=UTF-8");
+            // Générer du JSON
+            // Gson gson = new Gson();
+            // setPrettyPrinting pour que le JSON généré soit plus lisible
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            String gsonData = gson.toJson(resultat);
+            out.println(gsonData);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
